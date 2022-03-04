095afb368902d8937a86a0de8ac4ec1392d68257
Axik0 <xla.2@yandex.ru>
2022-03-04 18:04:37 +0300

    corrected create_all db 1/2

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ class Comment(db.Model):
     author = relationship("User", back_populates="comments")
     parent_post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+db.create_all()
 
 
 @login_manager.user_loader
